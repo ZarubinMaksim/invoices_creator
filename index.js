f2d3528a8ad69eba2be0c55b10afdbba6a95eb68
Maksim Zarubin <89940028777@ya.ru>
2025-09-10 16:34:12 +0700

    first

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
-
 const app = express();
 const PORT = 4000;
 
-// Главная страница
+// простой маршрут
 app.get('/', (req, res) => {
-    res.send(`
-        <h1>Привет! Это проект Invoices Creator</h1>
-        <p>Сервер работает и доступен через Nginx на /invoices/</p>
-    `);
+  res.send('Hello from Express!');
 });
 
-// Слушаем все внешние подключения
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Invoices server запущен на http://38.244.150.204:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });

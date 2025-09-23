75b7d73ff1481b21b67275e3f53e3ab1f4652d34
Maksim Zarubin <89940028777@ya.ru>
2025-09-23 17:32:38 +0700

    email_creator_date

--- backend.js
+++ backend.js
@@ -101,6 +101,7 @@ app.post(`/send-emails`, express.json(), async (req, res) => {
   const rows = req.body.rows || [];
   console.log('rows', rows)
   const results = [];
+  const row = rows[0];
   const dateObj = new Date(row.date.split("/").reverse().join("-"));
   // row.date = "01/09/2025" → "2025-09-01"
   const monthName = dateObj.toLocaleString("en-US", { month: "long" }); 
